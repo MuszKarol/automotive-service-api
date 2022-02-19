1c8fda550e5c6fb59d5c2282d58a9eab2f0e4c52
MuszKarol <karol.musz99@gmail.com>
2022-02-19 13:49:53 +0100

    Initial commit

---
+++ src/main/java/pl/KarolMusz/automotiveserviceapi/AutomotiveServiceApiApplication.java
@@ -0,0 +1,13 @@
+package pl.KarolMusz.automotiveserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomotiveServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomotiveServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/KarolMusz/automotiveserviceapi/AutomotiveServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.KarolMusz.automotiveserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomotiveServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
